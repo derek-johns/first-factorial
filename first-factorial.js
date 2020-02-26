2b339de3693b300f043b32d378fd62785bd70738
Derek Johnson <derekjohnson@Dereks-MBP.fios-router.home>
2020-02-26 00:05:12 -0500

    Completed challenge.

---
+++ first-factorial.js
@@ -0,0 +1,9 @@
+function FirstFactorial(num) {
+    let factorial = 1;
+    for (let i = 1; i <= num; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+console.log(FirstFactorial(5));
